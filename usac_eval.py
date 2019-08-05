bac878d20b456221f1d1017f72aab2bda963350a
Maier Josef <josef.maier.fl@ait.ac.at>
2019-08-05 17:27:49 +0200

    Finished evaluation nr 4 for USAC alg and refine

--- usac_eval.py
+++ usac_eval.py
@@ -566,9 +566,11 @@ def get_best_comb_and_th_1(**keywords):
                  # If true and a bar chart is chosen, the bars a filled with color and markers are turned off
                  'fill_bar': True
                  }
+    section_name = 'Smallest combined R \\& t errors and their ' + replaceCSVLabels(str(ret['grp_names'][-1]))
     tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], b_best_name),
-                                  'name': 'Smallest combined R \\& t errors and their ' +
-                                          replaceCSVLabels(str(ret['grp_names'][-1])),
+                                  'name': section_name,
+                                  'title': section_name,
+                                  'title_rows': section_name.count('\\\\'),
                                   'fig_type': fig_type,
                                   'plots': ['b_best'],
                                   'label_y': 'error',#Label of the value axis. For xbar it labels the x-axis
@@ -592,9 +594,11 @@ def get_best_comb_and_th_1(**keywords):
                                              replaceCSVLabels(str(ret['grp_names'][-1])) +
                                              ' which appears on top of each bar.'
                                   })
+    section_name = 'Worst combined R \\& t errors and their ' + replaceCSVLabels(str(ret['grp_names'][-1]))
     tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], b_worst_name),
-                                  'name': 'Worst combined R \\& t errors and their ' +
-                                          replaceCSVLabels(str(ret['grp_names'][-1])),
+                                  'name': section_name,
+                                  'title': section_name,
+                                  'title_rows': section_name.count('\\\\'),
                                   'fig_type': fig_type,
                                   'plots': ['b_worst'],
                                   'label_y': 'error',  # Label of the value axis. For xbar it labels the x-axis
@@ -682,9 +686,11 @@ def get_best_comb_inlrat_1(**keywords):
                  # If true and a bar chart is chosen, the bars a filled with color and markers are turned off
                  'fill_bar': True
                  }
+    section_name = 'Mean combined R \\& t errors over all ' + replaceCSVLabels(str(ret['grp_names'][-1]), True)
     tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], b_mean_name),
-                                  'name': 'Mean combined R \\& t errors over all ' +
-                                          replaceCSVLabels(str(ret['grp_names'][-1]), True),
+                                  'name': section_name,
+                                  'title': section_name,
+                                  'title_rows': section_name.count('\\\\'),
                                   'fig_type': fig_type,
                                   'plots': ['b_mean'],
                                   'label_y': 'error',  # Label of the value axis. For xbar it labels the x-axis
@@ -892,6 +898,7 @@ def get_best_comb_and_th_for_inlrat_1(**keywords):
                                       'name': section_name.replace('\\\\', ' '),
                                       # Title of the figure
                                       'title': section_name,
+                                      'title_rows': section_name.count('\\\\'),
                                       'fig_type': 'smooth',
                                       # Column name for charts based on the left y-axis
                                       'plots_l': ['b_min'],
@@ -955,6 +962,8 @@ def get_best_comb_and_th_for_inlrat_1(**keywords):
                  }
     tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], dataf_name),
                                   'name': 'Smallest Combined R \\& t Errors',
+                                  'title': 'Smallest Combined R \\& t Errors',
+                                  'title_rows': 0,
                                   'fig_type': 'ybar',
                                   'plots': ['b_min'],
                                   'label_y': 'error',  # Label of the value axis. For xbar it labels the x-axis
@@ -1045,7 +1054,7 @@ def get_best_comb_th_scenes_1(**keywords):
     partitions_legend = ' -- '.join([replaceCSVLabels(i) for i in ret['partitions'][:-1]])
     tmp2.index.name = partitions_ov
     tmp2 = tmp2.reset_index().set_index([it_pars_ov, partitions_ov]).unstack(level=0)
-    column_legend = [' -- '.join([replaceCSVLabels(i) for i in map(str, a)]) for a in tmp2.columns]
+    column_legend = [' -- '.join([i if 'b_min' in i else replaceCSVLabels(i) for i in map(str, a)]) for a in tmp2.columns]
     tmp2.columns = ['-'.join(map(str, a)) for a in tmp2.columns]
 
     b_mean_name = 'data_mean_min_RTerrors_and_corresp_' + ret['partitions'][-1] + '_for_opts_' + \
@@ -1063,7 +1072,7 @@ def get_best_comb_th_scenes_1(**keywords):
     right_cols = [a for a in tmp2.columns if ret['partitions'][-1] in a]
     left_cols = [a for a in tmp2.columns if 'b_min' in a]
     right_legend = [column_legend[i] for i, a in enumerate(tmp2.columns) if ret['partitions'][-1] in a]
-    left_legend = [column_legend[i] for i, a in enumerate(tmp2.columns) if 'b_min' in a]
+    left_legend = [column_legend[i].replace('b_min', 'R\\&t error') for i, a in enumerate(tmp2.columns) if 'b_min' in a]
     # Get data for tex file generation
     if len(tmp2.columns) > 10:
         fig_type = 'xbar'
@@ -1104,6 +1113,7 @@ def get_best_comb_th_scenes_1(**keywords):
                                   'name': section_name.replace('\\\\', ' '),
                                   # Title of the figure
                                   'title': section_name,
+                                  'title_rows': section_name.count('\\\\'),
                                   'fig_type': fig_type,
                                   # Column name for charts based on the left y-axis
                                   'plots_l': left_cols,
@@ -1131,6 +1141,49 @@ def get_best_comb_th_scenes_1(**keywords):
                                   'use_marks': True,
                                   'caption': caption
                                   })
+
+    for rc, lc, rl, ll in zip(right_cols, left_cols, right_legend, left_legend):
+        par_str = [i for i in rl.split(' -- ') if ret['partitions'][-1] not in i][0]
+        section_name = 'Smallest combined R \\& t errors and their ' + \
+                       replaceCSVLabels(str(ret['partitions'][-1]), True) + \
+                       '\\\\for parameters ' + par_str + \
+                       '\\\\and properties ' + ret['sub_title_partitions']
+
+        caption = 'Smallest combined R \\& t errors (left axis) and their ' + \
+                  replaceCSVLabels(str(ret['partitions'][-1]), True) + \
+                  ' (right axis) for parameters ' + par_str + \
+                  ' and properties ' + ret['sub_title_partitions'] + '.'
+        tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], b_mean_name),
+                                      # Name of the whole section
+                                      'name': section_name.replace('\\\\', ' '),
+                                      # Title of the figure
+                                      'title': section_name,
+                                      'title_rows': section_name.count('\\\\'),
+                                      'fig_type': 'ybar',
+                                      # Column name for charts based on the left y-axis
+                                      'plots_l': [lc],
+                                      # Label of the left y-axis.
+                                      'label_y_l': 'error',
+                                      # Column name for charts based on the right y-axis
+                                      'plots_r': [rc],
+                                      # Label of the right y-axis.
+                                      'label_y_r': replaceCSVLabels(str(ret['partitions'][-1])),
+                                      # Label of the x-axis.
+                                      'label_x': partitions_legend,
+                                      # Column name of the x-axis.
+                                      'plot_x': partitions_ov,
+                                      # Maximum and/or minimum y value/s on the left y-axis
+                                      'limits_l': None,
+                                      # Legend entries for the charts belonging to the left y-axis
+                                      'legend_l': [ll + ' (left axis)'],
+                                      # Maximum and/or minimum y value/s on the right y-axis
+                                      'limits_r': None,
+                                      # Legend entries for the charts belonging to the right y-axis
+                                      'legend_r': [rl + ' (right axis)'],
+                                      'legend_cols': 1,
+                                      'use_marks': True,
+                                      'caption': caption
+                                      })
 
     base_out_name = 'tex_min_mean_RTerrors_and_corresp_' + \
                     str(ret['partitions'][-1]) + '_for_opts_' + \
@@ -1160,72 +1213,76 @@ def get_best_comb_th_scenes_1(**keywords):
         ret['res'] += abs(res1)
         warnings.warn('Error occurred during writing/compiling tex file', UserWarning)
 
+    tex_infos = {'title': 'Smallest Combined R \\& t Errors ' + \
+                          ' for Parameters ' + ret['sub_title_it_pars'] + \
+                          ' and Properties ' + ret['sub_title_partitions'],
+                 'sections': [],
+                 # Builds an index with hyperrefs on the beginning of the pdf
+                 'make_index': False,
+                 # If True, the figures are adapted to the page height if they are too big
+                 'ctrl_fig_size': True,
+                 # If true, a pdf is generated for every figure and inserted as image in a second run
+                 'figs_externalize': False,
+                 # If true and a bar chart is chosen, the bars a filled with color and markers are turned off
+                 'fill_bar': True
+                 }
+    section_name = 'Smallest combined R \\& t errors ' + \
+                   '\\\\for parameters ' + ret['sub_title_it_pars'] + \
+                   '\\\\and properties ' + ret['sub_title_partitions']
+    caption = 'Smallest combined R \\& t errors and their ' + \
+              replaceCSVLabels(str(ret['partitions'][-1]), True) + \
+              ' (on top of each bar) for parameters ' + ret['sub_title_it_pars'] + \
+              ' and properties ' + ret['sub_title_partitions'] + '.'
+    tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], b_mean_name),
+                                  'name': section_name.replace('\\\\', ' '),
+                                  'title': section_name,
+                                  'title_rows': section_name.count('\\\\'),
+                                  'fig_type': 'ybar',
+                                  'plots': left_cols,
+                                  'label_y': 'error',  # Label of the value axis. For xbar it labels the x-axis
+                                  # Label/column name of axis with bars. For xbar it labels the y-axis
+                                  'label_x': partitions_legend,
+                                  # Column name of axis with bars. For xbar it is the column for the y-axis
+                                  'print_x': partitions_ov,
+                                  # Set print_meta to True if values from column plot_meta should be printed next to each bar
+                                  'print_meta': True,
+                                  'plot_meta': right_cols,
+                                  # A value in degrees can be specified to rotate the text (Use only 0, 45, and 90)
+                                  'rotate_meta': 45,
+                                  'limits': None,
+                                  # If None, no legend is used, otherwise use a list
+                                  'legend': left_legend,
+                                  'legend_cols': 1,
+                                  'use_marks': False,
+                                  # The x/y-axis values are given as strings if True
+                                  'use_string_labels': True,
+                                  'caption': caption
+                                  })
+    base_out_name = 'tex_min_mean_RTerrors_and_corresp_' + \
+                    str(ret['partitions'][-1]) + '_as_meta_for_opts_' + \
+                    '-'.join(ret['it_parameters']) + '_and_props_' + \
+                    ret['dataf_name_partition']
+    template = ji_env.get_template('usac-testing_2D_bar_chart_and_meta.tex')
+    rendered_tex = template.render(title=tex_infos['title'],
+                                   make_index=tex_infos['make_index'],
+                                   ctrl_fig_size=tex_infos['ctrl_fig_size'],
+                                   figs_externalize=tex_infos['figs_externalize'],
+                                   fill_bar=tex_infos['fill_bar'],
+                                   sections=tex_infos['sections'])
+    texf_name = base_out_name + '.tex'
+    pdf_name = base_out_name + '.pdf'
+    if ret['build_pdf'][1]:
+        res1 = compile_tex(rendered_tex,
+                           ret['tex_folder'],
+                           texf_name,
+                           tex_infos['make_index'],
+                           os.path.join(ret['pdf_folder'], pdf_name),
+                           tex_infos['figs_externalize'])
+    else:
+        res1 = compile_tex(rendered_tex, ret['tex_folder'], texf_name)
+    if res1 != 0:
+        ret['res'] += abs(res1)
+        warnings.warn('Error occurred during writing/compiling tex file', UserWarning)
 
-
-    # b_mean_best = b_mean.idxmin()
-    # alg_best = str(b_mean_best)
-    # b_best = float(b_mean.loc[b_mean_best])
-    #
-    # b_mean = b_mean.reset_index()
-    # b_mean.columns = ['options', 'b_mean']
-    # # Insert a tex line break for long options
-    # b_mean['options_tex'] = insert_opt_lbreak(ret['b'].columns)
-    #
-    #
-    # tex_infos = {'title': 'Mean Combined R \\& t Errors over all ' +
-    #                       replaceCSVLabels(str(ret['grp_names'][-1]), True, True) +
-    #                       ' for Parameter Variations of ' + ret['sub_title'],
-    #              'sections': [],
-    #              # Builds an index with hyperrefs on the beginning of the pdf
-    #              'make_index': False,
-    #              # If True, the figures are adapted to the page height if they are too big
-    #              'ctrl_fig_size': True,
-    #              # If true, a pdf is generated for every figure and inserted as image in a second run
-    #              'figs_externalize': False,
-    #              # If true and a bar chart is chosen, the bars a filled with color and markers are turned off
-    #              'fill_bar': True
-    #              }
-    # tex_infos['sections'].append({'file': os.path.join(ret['rel_data_path'], b_mean_name),
-    #                               'name': 'Mean combined R \\& t errors over all ' +
-    #                                       replaceCSVLabels(str(ret['grp_names'][-1]), True),
-    #                               'fig_type': fig_type,
-    #                               'plots': ['b_mean'],
-    #                               'label_y': 'error',  # Label of the value axis. For xbar it labels the x-axis
-    #                               # Label/column name of axis with bars. For xbar it labels the y-axis
-    #                               'label_x': 'Options',
-    #                               # Column name of axis with bars. For xbar it is the column for the y-axis
-    #                               'print_x': 'options_tex',
-    #                               # Set print_meta to True if values from column plot_meta should be printed next to each bar
-    #                               'print_meta': False,
-    #                               'plot_meta': [],
-    #                               # A value in degrees can be specified to rotate the text (Use only 0, 45, and 90)
-    #                               'rotate_meta': 0,
-    #                               'limits': None,
-    #                               # If None, no legend is used, otherwise use a list
-    #                               'legend': None,
-    #                               'legend_cols': 1,
-    #                               'use_marks': False,
-    #                               # The x/y-axis values are given as strings if True
-    #                               'use_string_labels': True,
-    #                               'caption': 'Mean combined R \\& t errors (error bars) over all ' +
-    #                                          replaceCSVLabels(str(ret['grp_names'][-1]), True) + '.'
-    #                               })
-    # ret['res'] = compile_2D_bar_chart('tex_mean_RT-errors_' + ret['grp_names'][-1], tex_infos, ret)
-    #
-    # main_parameter_name = 'USAC_opt_refine_ops_inlrat'
-    # # Check if file and parameters exist
-    # ppar_file, ret['res'] = check_par_file_exists(main_parameter_name, ret['res_folder'], ret['res'])
-    #
-    # with open(ppar_file, 'a') as fo:
-    #     # Write parameters
-    #     alg_comb_bestl = alg_best.split('-')
-    #     if len(ret['grp_names'][0:-1]) != len(alg_comb_bestl):
-    #         raise ValueError('Nr of refine algorithms does not match')
-    #     alg_w = {}
-    #     for i, val in enumerate(ret['grp_names'][0:-1]):
-    #         alg_w[val] = alg_comb_bestl[i]
-    #     yaml.dump({main_parameter_name: {'Algorithms': alg_w,
-    #                                      'b_best_val': b_best}},
-    #               stream=fo, Dumper=NoAliasDumper, default_flow_style=False)
     return ret['res']
 
