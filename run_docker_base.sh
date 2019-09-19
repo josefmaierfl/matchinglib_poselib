1b387c2f0b5f4da147062b24a8c72f52ff531d43
Matthias Schörghuber <matthias.schoerghuber@ait.ac.at>
2019-09-19 13:11:47 +0200

    added docker build environment

---
+++ run_docker_base.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+xhost +local:
+docker run -v `pwd`:/app  -it --runtime=nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix:ro git-service.ait.ac.at:8010/maierj/generatevirtualsequence /bin/bash
