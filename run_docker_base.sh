8dfa00502d3416023010e03a3d15eb92c53adfc2
Maier Josef <josef.maier.fl@ait.ac.at>
2019-09-24 14:11:46 +0200

    Some small changes

--- run_docker_base.sh
+++ run_docker_base.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 xhost +local:
-docker run -v `pwd`:/app  -it --runtime=nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix:ro git-service.ait.ac.at:8010/maierj/generatevirtualsequence /bin/bash
+docker run -v `pwd`:/app -v /home/maierj:/home/maierj -it --runtime=nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix:ro git-service.ait.ac.at:8010/maierj/generatevirtualsequence /bin/bash
