3c3df62be1a71f6ac4233765b387fa1eadee2ce7
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-03-04 11:42:15 +0100

    Small change

--- connect_and_prepare.sh
+++ connect_and_prepare.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
+if [ $# -ne 2 ]; then
   echo "Arguments are required"
   exit 1
 fi
 FIRST_ARG="$1"
+DRIVE_NAME="$2"
 #Connect to server and install dependencies
 scp -i "/home/maierj/work/aws/ait_calib_frankfurt.pem" /home/maierj/work/autocalib_test_package/prepare_os.sh "ubuntu@${FIRST_ARG}":/home/ubuntu
 ssh -i "/home/maierj/work/aws/ait_calib_frankfurt.pem" "ubuntu@${FIRST_ARG}" '/home/ubuntu/prepare_os.sh && exit'
