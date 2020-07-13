f24831ffff6012644abc229d4807701a51d38d8b
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-07-13 17:31:33 +0200

    Added MIT license

--- py_test_scripts/create_scenes.py
+++ py_test_scripts/create_scenes.py
@@ -1,5 +1,29 @@
 """
-Load configuration files and generate different scenes for testing using multiple CPU cores
+Released under the MIT License - https://opensource.org/licenses/MIT
+
+Copyright (c) 2020 Josef Maier
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Author: Josef Maier (josefjohann-dot-maier-at-gmail-dot-at)
+
+Description: Load configuration files and generate different scenes for testing using multiple CPU cores
 """
 import sys, re, numpy as np, math, argparse, os, pandas, time, subprocess as sp
 import ruamel.yaml as yaml
