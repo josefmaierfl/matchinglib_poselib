02d3717fb76168c3185c6d0c86b26bd58d9dbd04
Josef Maier <josefmaierfl@users.noreply.github.com>
2020-08-10 16:13:32 +0200

    Changed SBA build process from shared to static.

--- ci/thirdparty/sba-1.6/build/generic/build.sh
+++ ci/thirdparty/sba-1.6/build/generic/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-OPT_BUILD_SHARED_LIBS="ON"
+OPT_BUILD_SHARED_LIBS="OFF"
 FLAG_BUILD_SHARED_LIBS="-DBUILD_SHARED_LIBS=${OPT_BUILD_SHARED_LIBS}"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BLAS_P="$( cd $DIR && cd ../../../clapack-3.2.1/BLAS && pwd )"
+#BLAS_P="$( cd $DIR && cd ../../../clapack-3.2.1/BLAS && pwd )"
 pushd $DIR
 cd ../..
 mkdir lib
@@ -13,7 +13,7 @@ cd ../
 mkdir linux
 cd linux
 rm ./CMakeCache.txt
-cmake ../../ -DCMAKE_BUILD_TYPE=Release -DLAPACKBLAS_DIR="${BLAS_P}" ${FLAG_BUILD_SHARED_LIBS}
+cmake ../../ -DCMAKE_BUILD_TYPE=Release ${FLAG_BUILD_SHARED_LIBS}
 make -j "$(nproc)"
 if [ $? -ne 0 ]; then
     exit 1
@@ -25,7 +25,7 @@ cd ../
 mkdir linux_debug
 cd linux_debug
 rm ./CMakeCache.txt
-cmake ../../ -DCMAKE_BUILD_TYPE=Debug -DLAPACKBLAS_DIR="${BLAS_P}" ${FLAG_BUILD_SHARED_LIBS}
+cmake ../../ -DCMAKE_BUILD_TYPE=Debug ${FLAG_BUILD_SHARED_LIBS}
 make -j "$(nproc)"
 sudo make install
 if [ $? -ne 0 ]; then
